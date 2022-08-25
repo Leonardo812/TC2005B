d00c4d99f26d3d89a5347083f5d7778c68097ef8
Leonardo812 <a01707812@tec.mx>
2022-08-25 11:46:11 -0500

    Cambios lab 10

--- Lab10/index.js
+++ Lab10/index.js
@@ -1,20 +1,6 @@
 setTimeout(() => console.log("código asíncrono"), 4000);
-
 const { randomBytes } = require('crypto');
 const filesystem = require('fs');
-
-
-const arreglo = [5000, 60, 90, 100, 10, 20, 10000, 0, 120, 2000, 340, 1000, 50, 20000];
-
-//Imprime el arreglo en orden 
-//porque se ejecuta con los timeouts de los valores del arreglo
-for (let item of arreglo) {
-    setTimeout(() => {
-        console.log(item);
-    }, item);
-} 
-
-
 const http = require('http');
 
 const server = http.createServer( (request, response) => {    
@@ -33,73 +19,53 @@ const server = http.createServer( (request, response) => {
         filesystem.writeFileSync('M_hola.txt', 'Direccion Hola FUNCIONA');
         console.log("Direccion Hola FUNCIONA");
 
-    } else if (request.url === "/operacion") {
-      
-
-
-class Cuadrado{
-    constructor (l){
-      this.lado = l;
-    }
-
-            // Calcular Area
-            calcularArea(){
-                return (this.lado * this.lado);
-              }
-
-        // Getter area
-        get area() {
-            let c_Area =  "El area es: " + this.calcularArea() + "<br></br>"
-            return c_Area 
-          }
-
-
-
-
-
-    // Método cacular perímetro
-              calcPeri () {
-                return this.lado * 4 ;
+    } else if (request.url === "/trivia" && request.method === "GET") {
+        response.setHeader('Content-Type', 'text/html');
+        response.write("<!DOCTYPE html>");
+        response.write('<head><meta charset="UTF-8"></head>');
+        response.write("<h1>Pregunta al pulpo quién va a ganar</h1>");
+        response.write('<form action="trivia" method="POST">');
+        response.write('<fieldset>');
+        response.write('<legend>Equipos</legend>');
+        response.write('<label for="visitante">Visitante</label><input type="text" name="visitante" id="visitante">');
+        response.write('<br><br>');
+        response.write('<label for="local">Local</label><input type="text" name="local" id="local">');
+        response.write('</fieldset>');
+        response.write('<input type="submit" value="Adivina">');
+        response.write('</form>');
+        response.end();
+        filesystem.writeFileSync('M_trivia_get.txt', 'Direccion Trivia_get Funciona');
+        console.log("Direccion Trivia_get Funciona");
+        
+    } else if (request.url === "/trivia" && request.method === "POST") {
+        const datos = [];
+        request.on('data', (dato) => {
+            console.log(dato);
+            datos.push(dato);
+        });
+        return request.on('end', () => {
+            const datos_completos = Buffer.concat(datos).toString();
+            console.log(datos_completos);
+            const dato_visitante = datos_completos.split('&')[0].split('=')[1];
+            console.log(dato_visitante);
+            const dato_local = datos_completos.split('&')[1].split('=')[1];
+            console.log(dato_local);
+            response.setHeader('Content-Type', 'text/html');
+            response.write("<!DOCTYPE html>");
+            response.write('<head><meta charset="UTF-8"></head>');
+            response.write("<h1>El resultado es: </h1>");
+            if (Math.floor(Math.random() * 2) == 0) {
+                response.write('<h2>' + dato_visitante + '</h2>');
+            } else {
+                response.write('<h2>' + dato_local + '</h2>');
             }
+            filesystem.writeFileSync('M_trivia_post.txt', 'Direccion Trivia_post Funciona');
+            console.log("Direccion Trivia_post Funciona");
+            return response.end();
 
-     // Calcular Perimetro
-     get perimetro() {
-       let c_Per = "y su perímetro es: " + this.calcPeri() + "<br></br>"
-       return c_Per
-     }
-
-  }
-  
-  
-  var c = prompt ("Dame el lado del cuadrado: ");
-  var cuadrado = new Cuadrado (c)
-
-  document.getElementById("usuario").innerHTML = "La medida del lado que dio es " + c + " por lo que: " + "<br></br>"; 
-  document.getElementById("res6").innerHTML = cuadrado.area; 
-  document.getElementById("res7").innerHTML = cuadrado.perimetro; 
-  
-    response.write("<!DOCTYPE html>");
-    response.write('<head><meta charset="UTF-8"></head>');
-    response.write("<h1>Calculo de cuadrado</h1>"); 
-    response.write('<p> Se pide el lado de un cuadrado, posteriormente se calcula su Area y su perimetro:</p>'); 
-    response.write('<br></br>'); 
-    response.write('<h1>Calculo de Area y Perimetro de un Cuadrado:</h1>'); 
-    response.write('<img  src="https://www.smartick.es/blog/wp-content/uploads/cuadrado-222x201.png" width="200 "/>'); 
-    response.write('<br></br>'); 
-    response.write('<td> Introduce el lado del Cuadrado para calcular su area y Perimetro</td> '); 
-    response.write('<div id="usuario"></div>'); 
-    response.write('<div id="res6"></div>'); 
-    response.write(' <div id="res7"></div>'); 
-    filesystem.writeFileSync('M_trivia.txt', 'Direccion Operacion Funciona');
-    console.log("Direccion Operacion Funciona");
-    response.end();
-
-
-
-
-
-
+        });
     } 
+      
     
     
     
