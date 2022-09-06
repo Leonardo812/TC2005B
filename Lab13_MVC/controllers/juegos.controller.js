989791f1096e7df3db1cead91d3122ba56720cd9
Leonardo812 <a01707812@tec.mx>
2022-09-05 22:39:48 -0500

    lab 13 terminado - apoyo mvc

---
+++ Lab13_MVC/controllers/juegos.controller.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const jugador = require('../models/jugador.model');
+
+exports.getjuegos = (request, response, next) => {
+    response.sendFile(path.join(__dirname, '..', 'views', 'juegos.html'));
+};
+
+exports.postjuegos = (request, response, next) => {
+    console.log(request.body);
+    if ( request.body.videojuegos == 1 ) {
+        response.write("<h1>Gracias iniciado Fornite: </h1>");
+        response.write('<img src="https://cdn2.unrealengine.com/21br-metaimage-lineup-1920x1080-44acd5f27136.jpg" width="500 "/>')
+        
+    } else if ( request.body.videojuegos == 2 ) {
+        response.write("<h1>Gracias iniciado Dead by Daylight: </h1>");
+        response.write('<img src="https://cdn1.epicgames.com/offer/611482b8586142cda48a0786eb8a127c/EGS_DeadbyDaylight_BehaviourInteractive_S1_2560x1440-a32581cf9948a9a2e24b2ff15c1577c7" width="500 "/>')
+            
+    } else if ( request.body.videojuegos == 3 ) {
+        response.write("<h1>Gracias iniciado League of Legends : </h1>");
+        response.write('<img src="https://cdn1.epicgames.com/salesEvent/salesEvent/EGS_LeagueofLegends_RiotGames_S1_2560x1440-ee500721c06da3ec1e5535a88588c77f"  width="500"/>')
+          
+    } else if ( request.body.videojuegos == 4 ){
+        response.write("<h1>Gracias iniciado Valorant: </h1>");
+        response.write('<img src="https://i.blogs.es/78bc94/valorant2/1366_2000.jpg" width="500 "/>')
+               
+    } else {
+        response.write("<h1>Lo sentimos, juego no encontrado: </h1>");
+        response.write('<img src="https://sitechecker.pro/wp-content/uploads/2017/12/404.png" width="500 "/>')
+    }
+};
--- material_de_apoyo/Lab13_MVC/app.js
+++ material_de_apoyo/Lab13_MVC/app.js
@@ -11,25 +11,36 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.urlencoded({extended: false}));
 
+//Middleware
+app.use((request, response, next) => {
+    console.log('Middleware!');
+    next(); //Le permite a la petición avanzar hacia el siguiente middleware
+});
+
+app.use((request, response, next) => {
+    console.log("Segundo middleware");
+    next();
+});
+
+const rutas_trivia = require('./routes/trivia.routes');
+app.use('/paul', rutas_trivia);
+
 const rutas_juego = require('./routes/juego.routes');
 app.use('/juego', rutas_juego);
 
+app.use('/hola/lalo', (request, response, next) => {
+    response.send('Hola! desde la ruta "/hola/lalo"'); 
+});
 
 app.use('/hola', (request, response, next) => {
     response.send('Hola! desde la ruta "/hola"'); 
 });
 
-
 app.get('/info', (request, response, next) => {
-    response.render(path.join(__dirname, 'views', 'index.ejs'));
-});
-
-app.get("/",(request, response) => {
-    let html =  '<!DOCTYPE html>' +
-    '<head><meta charset="UTF-8"></head>'+
-    "<h1> No hay lugar como el hogar</h1>"+
-    '<img src = " https://upload.wikimedia.org/wikipedia/commons/thumb/e/ee/Revista_%C2%A1Hola%21_logo.svg/1200px-Revista_%C2%A1Hola%21_logo.svg.png" width="500 "/>';
-    response.send(html); //Manda la respuesta
+    console.log(path.join(__dirname));
+    console.log(path.join(__dirname, '..'));
+    console.log(path.join(__dirname, '..', 'views', 'index.html'));
+    response.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
 app.use((request, response, next) => {
